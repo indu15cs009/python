db4f53c9c4ca09e289eb1371c7a3d9357f923c94
indu15cs009 <36760334+indu15cs009@users.noreply.github.com>
2018-02-23 15:44:48 +0530

    Create recerse.c

---
+++ recerse.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+int main()
+{
+int i,n,t1=0,t2=5, term;
+printf("enter the number");
+scanf("%d",&n);
+printf("fibonnaci series");
+for(i=0;i<=n;++i)
+{
+printf("%d",t1);
+t1=t2;
+t2=term;
+}
+return 0;
+}
+
+
